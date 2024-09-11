3d41a23ea3d6de61c3404b1ccfb5da4bb3aa88b7
Tanyse <tanyse.lima02@gmail.com>
2024-09-10 23:19:14 -0300

    Rotas servicos

--- src/servicos/servicos.controller.ts
+++ src/servicos/servicos.controller.ts
@@ -15,27 +15,27 @@ import { UpdateServicoDto } from './dto/update-servico.dto';
 export class ServicosController {
   constructor(private readonly servicosService: ServicosService) {}
 
-  @Post()
+  @Post('adicionar')
   create(@Body() createServicoDto: CreateServicoDto) {
     return this.servicosService.create(createServicoDto);
   }
 
-  @Get()
+  @Get('listar')
   findAll() {
     return this.servicosService.findAll();
   }
 
-  @Get(':id')
+  @Get('buscarId/:id')
   findOne(@Param('id') id: string) {
     return this.servicosService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('atualizar/:id')
   update(@Param('id') id: string, @Body() updateServicoDto: UpdateServicoDto) {
     return this.servicosService.update(+id, updateServicoDto);
   }
 
-  @Delete(':id')
+  @Delete('excluir/:id')
   remove(@Param('id') id: string) {
     return this.servicosService.remove(+id);
   }
