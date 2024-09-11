8550e9d73b80f6e74dfc11ed9e9538168a9d5ebe
Tanyse <tanyse.lima02@gmail.com>
2024-09-10 23:02:04 -0300

    Rotas equipamentos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EquipamentosModule } from './equipamentos/equipamentos.module';
+import { EstoqueModule } from './estoque/estoque.module';
+import { DespesaModule } from './despesa/despesa.module';
+import { ServicosModule } from './servicos/servicos.module';
+import { ColaboradorModule } from './colaborador/colaborador.module';
 
 @Module({
-  imports: [EquipamentosModule],
+  imports: [
+    EquipamentosModule,
+    EstoqueModule,
+    DespesaModule,
+    ServicosModule,
+    ColaboradorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/equipamentos/equipamentos.controller.ts
+++ src/equipamentos/equipamentos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { EquipamentosService } from './equipamentos.service';
 import { CreateEquipamentoDto } from './dto/create-equipamento.dto';
 import { UpdateEquipamentoDto } from './dto/update-equipamento.dto';
@@ -7,27 +15,30 @@ import { UpdateEquipamentoDto } from './dto/update-equipamento.dto';
 export class EquipamentosController {
   constructor(private readonly equipamentosService: EquipamentosService) {}
 
-  @Post()
+  @Post('adicionar')
   create(@Body() createEquipamentoDto: CreateEquipamentoDto) {
     return this.equipamentosService.create(createEquipamentoDto);
   }
 
-  @Get()
+  @Get('listar')
   findAll() {
     return this.equipamentosService.findAll();
   }
 
-  @Get(':id')
+  @Get('buscarId/:id')
   findOne(@Param('id') id: string) {
     return this.equipamentosService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEquipamentoDto: UpdateEquipamentoDto) {
+  @Patch('atualizar/:id')
+  update(
+    @Param('id') id: string,
+    @Body() updateEquipamentoDto: UpdateEquipamentoDto,
+  ) {
     return this.equipamentosService.update(+id, updateEquipamentoDto);
   }
 
-  @Delete(':id')
+  @Delete('excluir/:id')
   remove(@Param('id') id: string) {
     return this.equipamentosService.remove(+id);
   }
