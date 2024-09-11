50fcfa2ef25152df3dd4a36db43e3728577edd96
Tanyse <tanyse.lima02@gmail.com>
2024-09-10 23:19:46 -0300

    Rotas estoque

--- src/estoque/estoque.controller.ts
+++ src/estoque/estoque.controller.ts
@@ -15,27 +15,27 @@ import { UpdateEstoqueDto } from './dto/update-estoque.dto';
 export class EstoqueController {
   constructor(private readonly estoqueService: EstoqueService) {}
 
-  @Post()
+  @Post('adicionar')
   create(@Body() createEstoqueDto: CreateEstoqueDto) {
     return this.estoqueService.create(createEstoqueDto);
   }
 
-  @Get()
+  @Get('listar')
   findAll() {
     return this.estoqueService.findAll();
   }
 
-  @Get(':id')
+  @Get('buscarId/:id')
   findOne(@Param('id') id: string) {
     return this.estoqueService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('atualizar/:id')
   update(@Param('id') id: string, @Body() updateEstoqueDto: UpdateEstoqueDto) {
     return this.estoqueService.update(+id, updateEstoqueDto);
   }
 
-  @Delete(':id')
+  @Delete('excluir/:id')
   remove(@Param('id') id: string) {
     return this.estoqueService.remove(+id);
   }
